ddca7826ebd820176345146ded6552d041eddc07
Gowri4622 <gowrihifi@gmail.com>
2023-05-27 11:05:50 +0530

    Updated and included MVC architecture to get method

--- todo_items.js
+++ todo_items.js
@@ -1,8 +1,6 @@
 const express=require('express')
 const router = express.Router()
-
-router.get('/',(request,response)=>{
-    response.send("List of all to items")
-})
+const {getAllToDoItems}=require('./controllers/todo_items')
+router.route('/').get(getAllToDoItems)
 
 module.exports=router
